5b49f10f9d7a9f49153197b3d24210585679258a
Muhammad Juniarto <cc.magang94@toyota.co.id>
2025-03-14 07:39:34 +0700

    add documentation for the LockType and the systemState

--- src/enum/SystemState.h
+++ src/enum/SystemState.h
@@ -1,12 +1,18 @@
 #ifndef SYSTEM_STATE_H
 #define SYSTEM_STATE_H
 
+
+/**
+ * @enum SystemState
+ * @brief Enum representing the type of access control (RFID or Fingerprint).
+ * 
+ */
 enum SystemState {
-    RUNNING,
-    ENROLL_FP,
-    ENROLL_RFID,
-    DELETE_FP,
-    DELETE_RFID,
+    RUNNING,            /* The normal state of the system operation                                 */
+    ENROLL_FP,          /* The state to change system transtition to register new Fingerprint       */
+    ENROLL_RFID,        /* The state to change system transtition to register new RFID              */
+    DELETE_FP,          /* The state to change system transtition to delete Fingerprint access      */
+    DELETE_RFID,        /* The state to change system transtition to delete RFID access             */
 };
 
 #endif
